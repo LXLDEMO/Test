1b0e7b7fd63cbe7bef70ef408c1ea6b3dc940c01
李晓璐 <44945785+LXLDEMO@users.noreply.github.com>
2020-12-05 00:10:31 +0800

    just test

---
+++ suanfa10-9/src/Test.java
@@ -0,0 +1,58 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Test {
+
+	/**
+	 * java中的输出很耗，可以通过stringBuffer来拼接最后进行输出
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+//		while (scanner.hasNext()) {
+//			int num = scanner.nextInt();
+//			//输出对应的二进制 不加前导0
+//			System.out.println(Integer.toBinaryString(scanner.nextInt()));
+//		}
+		
+//		int N,K;
+//		N = scanner.nextInt();
+//		K=scanner.nextInt();
+//		boolean[] arr = new boolean[N+1];
+//		for (int i = 0; i <=K; i++) {
+//			for (int j = i; j <=N; j+=i) {
+//				arr[j] = !arr[j];
+//			}
+//		}
+//		for (int i = 1; i <=N; i++) {
+//			if (arr[i]) {
+//				System.out.printf("%d ",arr[i]);
+//			}
+//			
+//		}
+		
+//		int T,N;
+//		T=scanner.nextInt();
+//		while (T-->0) {
+//			N = scanner.nextInt();
+//			int[] arr = new int[2*N];
+//			for (int i = 0; i < 2*N; i++) {
+//				arr[i]  = scanner.nextInt();
+//			}
+//			Arrays.sort(arr);
+//			//默认初始值是false
+//			boolean flags[] = new boolean[200];
+//			for (int i = 0; i < 2*N; i++) {
+//				if (!flags[arr[i]]) {
+//					System.out.printf("%d",arr[i]);
+//					flags[arr[i]] = true;
+//				}
+//			}
+//		}
+//
+	}
+	
+
+}
